ae854bef26f97fd0d4d9e9b493780c0dda263e7a
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2023-12-24 23:00:37 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request
+import openai
+
+client = openai.Client(api_key="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")
+
+my_assistant = client.beta.assistants.retrieve("asst_7bHMEyumWTDCjprU3gpBjVqs")
+#print(my_assistant)
+# lastQuery = ""
+# def createThread(query):
+#   run = client.beta.threads.create(
+#     messages=[
+#       {
+#         "role": "user",
+#         "content": query
+#       }
+#     ]
+#   )
+my_thread = client.beta.threads.create()
+
+my_message = client.beta.threads.messages.create(
+  thread_id=my_thread.id,
+  role= 'user',
+  content= 'Space History'
+)
+
+my_run = client.beta.threads.runs.create(
+  thread_id=my_thread.id,
+  assistant_id=my_assistant.id
+)
+print(my_run)
+while my_run.status in ["queued", "in_progress"]:
+  keep_retrieving_run = client.beta.threads.runs.retrieve(
+      thread_id=my_thread.id,
+      run_id=my_run.id
+  )
+  print(f"Run status: {keep_retrieving_run.status}")
+
+  if keep_retrieving_run.status == "completed":
+      print("\n")
+
+      # Step 6: Retrieve the Messages added by the Assistant to the Thread
+      all_messages = client.beta.threads.messages.list(
+          thread_id=my_thread.id
+      )
+
+      print("------------------------------------------------------------ \n")
+
+      print(f"User: {my_assistant.content[0].text.value}")
+      print(f"Assistant: {all_messages.data[0].content[0].text.value}")
+
+      break
+  elif keep_retrieving_run.status == "queued" or keep_retrieving_run.status == "in_progress":
+      pass
+  else:
+      print(f"Run status: {keep_retrieving_run.status}")
+      break
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route("/query/<query>")
+# def respondToQuery(query):
+#   if query == lastQuery:
+
+    
