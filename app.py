8d200cfb8f48683f9ac31beaf5e52592f2047d1e
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-01-20 13:07:53 -0800

    Big Update

--- app.py
+++ app.py
@@ -11,37 +11,47 @@ app = Flask(__name__)
 @app.route("/query/<query>")
 def respondToQuery(query):
     my_thread = client.beta.threads.create()
-    
+
     my_message = client.beta.threads.messages.create(
-      thread_id=my_thread.id,
-      role= 'user',
-      content= query
-    )
+          thread_id=my_thread.id,
+          role= 'user',
+          content= query
+        )
     
     my_run = client.beta.threads.runs.create(
-      thread_id=my_thread.id,
-      assistant_id=my_assistant.id
-    )
-    
-    #all_messages = ""
-    keep_retrieving_run = client.beta.threads.runs.retrieve(
-    thread_id=my_thread.id,
-    run_id=my_run.id
-  )
-    print(f"Run status: {keep_retrieving_run.status}")
+  thread_id=my_thread.id,
+  assistant_id=my_assistant.id,
+  instructions= client.beta.threads.thread_id.instructions
+)
+    print(f"This is the run object: {my_run} \n")
 
-    if keep_retrieving_run.status == "completed":
-      print("\n")
+    # Step 5: Periodically retrieve the Run to check on its status to see if it has moved to completed
+    while my_run.status in ["queued", "in_progress"]:
+        keep_retrieving_run = client.beta.threads.runs.retrieve(
+            thread_id=my_thread.id,
+            run_id=my_run.id
+        )
+        print(f"Run status: {keep_retrieving_run.status}")
 
-      # Step 6: Retrieve the Messages added by the Assistant to the Thread
-      all_messages = client.beta.threads.messages.list(
-          thread_id=my_thread.id
-      )
+        if keep_retrieving_run.status == "completed":
+            print("\n")
 
-      print("------------------------------------------------------------ \n")
+            # Step 6: Retrieve the Messages added by the Assistant to the Thread
+            all_messages = client.beta.threads.messages.list(
+                thread_id=my_thread.id
+            )
 
-      #print(client.beta.threads.messages.list(thread_id=my_thread.id))
-    return jsonify(client.beta.threads.messages.list(thread_id=my_thread))
+            print("------------------------------------------------------------ \n")
+
+            print(f"User: {my_message.content[0].text.value}")
+            print(f"Assistant: {all_messages.data[0].content[0].text.value}")
+
+            break
+        elif keep_retrieving_run.status == "queued" or keep_retrieving_run.status == "in_progress":
+            pass
+        else:
+            print(f"Run status: {keep_retrieving_run.status}")
+            break
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0')
